86b9ac3f899a3d8180b77c9a7c9c6054decce92f
Fizi99 <anton@m-fitzenreiter.de>
2022-01-29 18:44:14 +0100

    game stuff

--- server.js
+++ server.js
@@ -47,6 +47,8 @@ app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use('css', express.static(__dirname + 'public/css'));
 app.use('js', express.static(__dirname + 'public/js'));
+app.use('fonts', express.static(__dirname + 'public/fonts'));
+app.use('images', express.static(__dirname + 'public/images'));
 //app.use(express.static('../HacknDashAWD/resources/css/' + 'game.css'));
 
 //displayt index.ejs 
